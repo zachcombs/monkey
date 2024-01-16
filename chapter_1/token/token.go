15e7f9f1eb60627ca28aaf61d7de724dbe95ce6f
Zach Combs <zacharycombstwo@gmail.com>
2024-01-16 16:29:14 -0500

    Added possible token types

--- chapter_1/token/token.go
+++ chapter_1/token/token.go
@@ -5,3 +5,28 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	// Special Types
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	//Identifiers and Literals
+	IDENT  = "IDENT"
+	INT    = "INT"
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
